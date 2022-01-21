9f96b3e8a9ac037ff2e91d19cfa284f1de2d8ea6
igor <igro.sia@yandex.ru>
2022-01-21 19:59:10 +0300

    Initial Commit

---
+++ GraphicAndAnimationSwiftUI/GraphicAndAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicAndAnimationSwiftUIApp.swift
+//  GraphicAndAnimationSwiftUI
+//
+//  Created by Игорь Сысоев on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphicAndAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
